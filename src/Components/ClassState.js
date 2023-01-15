13195a398dbcde96086b0b79a9b237050a79916e
Paublo Martínez <pamartinez@vdkit.net>
2023-01-14 20:43:43 -0600

    Clase no. 8

--- src/Components/ClassState.js
+++ src/Components/ClassState.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+const SECURITY_CODE = 'paradigma';
+
 class ClassState extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
+            value : '',
             error: false,
             loading: false
         }
@@ -14,7 +17,13 @@ class ClassState extends React.Component {
     componentDidUpdate() {
         if (this.state.loading) {
             setTimeout(() => {
-                this.setState({ loading: false })
+
+                if(SECURITY_CODE===this.state.value){
+                    this.setState({ error: false, loading: false })                    
+                }else{
+                    this.setState({error: true, loading: false})
+                }
+
             }, 2000)
         }
     }
@@ -24,13 +33,22 @@ class ClassState extends React.Component {
             <div>
                 <h2>Eliminar {this.props.nombre}</h2>
                 <p>Por favor, escribe el código de seguridad</p>
-                {this.state.error && (
+                { (this.state.error && !this.state.loading) && (
                     <p>El código es incorrecto</p>
                 )}
                 {this.state.loading && (
                     <p>Cargando...</p>
                 )}
-                <input placeholder='Codigo de seguridad' />
+                <input 
+                    placeholder='Codigo de seguridad' 
+                    value={this.state.value}
+                    onChange={
+                        (event) => {
+                            this.setState({value : event.target.value})
+                        }
+                    }
+                />
+
                 <button
                     onClick={() => this.setState({ loading: true })}
                 >Comparar</button>
--- src/Components/UseState.js
+++ src/Components/UseState.js
@@ -1,14 +1,27 @@
 import React from 'react'
 
+const SECURITY_CODE = 'paradigma';
+
 export const UseState = ({ nombre }) => {
 
+    const [value, setValue] = React.useState('')
     const [error, setError] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
+
 
     React.useEffect(() => {
         if (loading) {
             setTimeout(() => {
-                setLoading(false)
+
+                if(value === SECURITY_CODE){
+                    setLoading(false)
+                    setError(false)
+                }else{
+                    setError(true)
+                    setLoading(false)
+                }
+
+                
             }, 2000)
         }
     }, [loading])
@@ -24,9 +37,15 @@ export const UseState = ({ nombre }) => {
             {loading && (
                 <p>Cargando...</p>
             )}
-            <input placeholder='Codigo de seguridad' />
+            <input 
+                placeholder='Codigo de seguridad' 
+                value={value}
+                onChange={(event) => {
+                    setValue(event.target.value)
+                }}
+            />
             <button
-                onClick={() => setLoading(true)}
+                onClick={() => {setLoading(true); setError(false)}}
             >Comparar</button>
         </div>
     )
